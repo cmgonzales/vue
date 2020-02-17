29f48d0fe912d3e317387eb89cb43f2b152b9f8b
Christopher Gonzales <cm.gonzales@yahoo.com>
2020-02-16 18:53:24 -0800

    second

--- app.js
+++ app.js
@@ -11,7 +11,35 @@ new Vue({
             this.gameIsRunning = true,
             this.playerHealth = 100,
             this.monsterHealth = 100
-        }
+        },
+        attack: function(){
+            this.monsterHealth -= this.calculateDamage(3,10);
+            
+            if(this.monsterHealth <= 0){
+                alert('you won')
+                this.gameIsRunning = false;
+            }
+            this.playerHealth -= this.calculateDamage(5,12)
+
+            if(this.playerHealth <= 0){
+                alert('you lost')
+                this.gameIsRunning = false;
+            }
+
+        },
+        specialAttack: function(){
+
+        },
+        heal: function(){
+
+        },
+        giveUp: function(){
+
+        } ,
+        calculateDamage: function(min,max){
+          return  Math.max(Math.floor(Math.random() * max) + 1, min)
+        }  
+
     }
 })
 
